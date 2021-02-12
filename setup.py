c80246456cd0cf5cfd18aaad39711dedb88a0eb7
wnma3mz <wnma3mz@163.com>
2021-02-12 09:55:23 +0800

    update proxies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wechat_msg",
-    version="0.2",
+    version="0.3",
     author="wnma3mz",
     author_email="",
     description="wechat send message",
--- wechat_msg/WeChat.py
+++ wechat_msg/WeChat.py
@@ -5,28 +5,35 @@ import os
 
 class Authorize(object):
     # Get Token
-    def __init__(self, corpid, corpsecret):
+    def __init__(self, corpid, corpsecret, proxies=None):
         # corpid    : wechat id
         # corpsecret: wechat secret
         self.corpid = corpid
         self.corpsecret = corpsecret
-        self.token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}'
+        self.token_url = (
+            "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}"
+        )
+        self.proxies = proxies
 
     def get_token(self):
         # get_token
         json_ = requests.get(
-            self.token_url.format(self.corpid, self.corpsecret)).json()
-        return json_['access_token']
+            self.token_url.format(self.corpid, self.corpsecret), proxies=self.proxies
+        ).json()
+        return json_["access_token"]
 
 
 class Send(object):
     # Send Message (text, image, video, voice...)
-    def __init__(self, agentid, token, data=None):
+    def __init__(self, agentid, token, data=None, proxies=None):
         # agentid: application id
         # token  : Authorize get token
         self.agentid = agentid
         self.token = token
-        self.send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}'
+        self.send_url = (
+            "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}"
+        )
+        self.proxies = proxies
 
         if data != None:
             self.data = data
@@ -36,7 +43,7 @@ class Send(object):
                 "safe": 0,
                 "enable_id_trans": 0,
                 "enable_duplicate_check": 0,
-                "duplicate_check_interval": 1800
+                "duplicate_check_interval": 1800,
             }
 
     def _type(self, to_type, name):
@@ -49,8 +56,9 @@ class Send(object):
             )
 
     def _send(self):
-        s = requests.post(self.send_url.format(self.token),
-                          json=self.data).json()
+        s = requests.post(
+            self.send_url.format(self.token), json=self.data, proxies=self.proxies
+        ).json()
         return s
 
     def send_msg(self, content, name="", to_type="touser"):
@@ -64,11 +72,7 @@ class Send(object):
         self.data["text"]["content"] = content
         return self._send()
 
-    def send_img(self,
-                 media_id=None,
-                 img_path=None,
-                 name="",
-                 to_type="touser"):
+    def send_img(self, media_id=None, img_path=None, name="", to_type="touser"):
         # media_id: wechat temp media id (three days)
         # img_path: local image path
         # TO-DO:    suport img url
@@ -101,13 +105,9 @@ class Send(object):
         self.data["voice"]["media_id"] = voice_id
         return self._send()
 
-    def send_video(self,
-                   video_id,
-                   video_path,
-                   title="",
-                   description="",
-                   name="",
-                   to_type="touser"):
+    def send_video(
+        self, video_id, video_path, title="", description="", name="", to_type="touser"
+    ):
         # video_id: wechat temp voice id (three days)
         # video_path: local voice path
         assert name != ""
@@ -126,11 +126,7 @@ class Send(object):
 
         return self._send()
 
-    def send_file(self,
-                  file_id=None,
-                  file_path=None,
-                  name="",
-                  to_type="touser"):
+    def send_file(self, file_id=None, file_path=None, name="", to_type="touser"):
         # file_id: wechat temp media id (three days)
         # file_path: local file path
         # TO-DO:    suport file url
@@ -182,37 +178,40 @@ class Upload(object):
     # Upload Data(image, video, voice...)
     # TO-DO: Upload video, voice...
 
-    def __init__(self, token):
+    def __init__(self, token, proxies=None):
         self.token = token
         self.headers = {
             "content-type": "multipart/form-data",
         }
-        self.upload_url = 'https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token={}&type={}'
+        self.upload_url = (
+            "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token={}&type={}"
+        )
+        self.proxies = proxies
 
     def upload_img(self, img_path):
         # img_path: local image path
         # TO-DO:    suport img url
-        type_ = 'image'
-        with open(img_path, 'rb') as f:
+        type_ = "image"
+        with open(img_path, "rb") as f:
             f_img_b = f.read()
 
-        files = {'image': (os.path.basename(img_path), f_img_b, 'image/jpg')}
-        s = requests.post(self.upload_url.format(self.token, type_),
-                          files=files).json()
-        return s['media_id']
+        files = {"image": (os.path.basename(img_path), f_img_b, "image/jpg")}
+        s = requests.post(
+            self.upload_url.format(self.token, type_), files=files, proxies=self.proxies
+        ).json()
+        return s["media_id"]
 
     def upload_voice(self, voice_path):
         # voice_path: local voice path
-        type_ = 'voice'
-        with open(voice_path, 'rb') as f:
+        type_ = "voice"
+        with open(voice_path, "rb") as f:
             f_voice_b = f.read()
 
-        files = {
-            'voice': (os.path.basename(voice_path), f_voice_b, 'voice/amr')
-        }
-        s = requests.post(self.upload_url.format(self.token, type_),
-                          files=files).json()
-        return s['media_id']
+        files = {"voice": (os.path.basename(voice_path), f_voice_b, "voice/amr")}
+        s = requests.post(
+            self.upload_url.format(self.token, type_), files=files, proxies=self.proxies
+        ).json()
+        return s["media_id"]
 
     def upload_video(self, video_path):
         # TO-DO
